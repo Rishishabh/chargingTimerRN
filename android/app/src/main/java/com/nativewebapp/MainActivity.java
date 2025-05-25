64e1a2ee803624fedf1f6a0537b4c7163c1e03db
Rishishabh <rishabhnahata1@gmail.com>
2025-05-26 00:41:15 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/nativewebapp/MainActivity.java
+++ android/app/src/main/java/com/nativewebapp/MainActivity.java
@@ -2,6 +2,8 @@
 package com.nativewebapp;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import androidx.appcompat.R;
 
 public class MainActivity extends ReactActivity {
     @Override
