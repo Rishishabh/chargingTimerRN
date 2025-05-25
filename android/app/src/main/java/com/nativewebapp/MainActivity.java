de9442e54e49453d4f501f510f21ad74e769ef32
Rishishabh <rishabhnahata1@gmail.com>
2025-05-26 00:36:45 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/nativewebapp/MainActivity.java
+++ android/app/src/main/java/com/nativewebapp/MainActivity.java
@@ -5,6 +5,13 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Set theme before super.onCreate
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
     protected String getMainComponentName() {
         return "nativewebapp";
     }
