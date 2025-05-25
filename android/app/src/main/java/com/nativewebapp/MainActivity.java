e512a60795775c19245f64158647ec85bfb8185b
Rishishabh <rishabhnahata1@gmail.com>
2025-05-26 00:57:42 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/nativewebapp/MainActivity.java
+++ android/app/src/main/java/com/nativewebapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ReactActivity {
         // Set theme before super.onCreate
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
     }
     
     @Override
